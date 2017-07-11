8de55f4040d67bd90f4d7c9208dafa4fe683da47
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-07-11 22:06:31 +0530

    LastQuestion_Function_Overloading
    Though not actually implementable or possible unless additional module/s is/are used.

---
+++ lastquest_funcoverload_u1c2_ncert.py
@@ -0,0 +1,34 @@
+"""FUNCTION OVERLOADING IS NOT POSSIBLE IN PYTHON"""
+"""However, if it was possible, the following code would work."""
+def volume(a):
+    volume=a**2
+    return volume
+def volume(a,b,c): #b-height
+    volume=a*b*c
+    return volume
+def volume(a,b): #a-radius|b-height
+    from math import pi
+    volume= pi*(a**2)*b
+    return volume
+a=raw_input("Enter side1: ")
+b=raw_input("Enter side2: ")
+c=raw_input("Enter side3: ")
+ta=bool(a)
+tb=bool(b)
+tc=bool(c)
+if ta:
+    a=float(a)
+    if not (tb and tc):
+        volume(a)
+    elif tb and (not tc):
+        b=float(b)
+        volume(a,b)
+    elif (tb and tc):
+        b=float(b)
+        c=float(c)
+        volume(a,b,c)
+
+
+
+
+"""It's possible using module/s: https://pypi.python.org/pypi/overload"""
